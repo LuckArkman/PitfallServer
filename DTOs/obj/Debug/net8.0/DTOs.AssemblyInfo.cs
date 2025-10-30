b078bc0df7754fcfd4d2facf46c8c6a39e5bc481
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-10-30 10:35:08 -0300

    up

--- DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
+++ DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af0ae7b8869152df0a2d85a83cb61c1e3db3128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817261479a65f00206804470fa1eed606cddbb37")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
