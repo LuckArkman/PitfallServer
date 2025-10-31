72f865b2c6b81a24e73b966359e66ec967b72c73
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-10-31 19:31:00 -0300

    up

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+073065b0000adc6a2025cf2165b84d57d1d0ee6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9030afe2f6fc04155499d7c0ac0c59e547928411")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
