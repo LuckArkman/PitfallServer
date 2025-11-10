a9a79dcb2d5051b557a20edb15f94e450e801d05
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-11-10 13:53:26 -0300

    update

--- DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
+++ DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f89cf21f382f13a903ae1316325662b0e56709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8773127499eb85c3c7cb12de80ca41b36643406")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f89cf21f382f13a903ae1316325662b0e56709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8773127499eb85c3c7cb12de80ca41b36643406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
