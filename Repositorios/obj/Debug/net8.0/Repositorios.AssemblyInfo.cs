fc8095e20f97b380a2358fe66c9c7bbd7513946a
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-01-03 01:21:08 -0300

    up

--- Repositorios/obj/Debug/net8.0/Repositorios.AssemblyInfo.cs
+++ Repositorios/obj/Debug/net8.0/Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf480f575fbb4ff7cc3ef710417bd804cb330a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0162a7530be2ba6f306f955a4efa39cb1ef7008d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
